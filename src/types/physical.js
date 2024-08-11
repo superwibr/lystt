c05257f43f2bda9bfd009a5bb16634a936e3eb7d
superwibr <superwibr@gmail.com>
2024-08-11 14:41:02 -0400

    physics rewrite

--- src/types/physical.js
+++ src/types/physical.js
@@ -5,150 +5,212 @@ const _rectifyRotation = (rotation) => {
 
 const _snapRotation = (rotation) => {
 	const rectified = _rectifyRotation(rotation);
-	const nearest = Math.round(rectified / 100) * 100;
+	const nearest = Math.round(rectified * 0.01) * 100;
 	return nearest % 400;
 };
 
 const _validateVector = (vector) => {
-	if (Array.isArray(vector) && vector.length === 2) {
-		const [x, y] = vector.map(Number);
-		if (!isNaN(x) && !isNaN(y)) {
-			return [x, y];
+	const sdims = vector.length;
+	if (Array.isArray(vector)) {
+		const numvec = vector.map(Number);
+		if (!numvec.some(n => isNaN(n))) {
+			return numvec;
 		}
 	}
-	return [0, 0];
+	return Array(sdims).fill(0);
 };
 
-const _validateVector3D = (vector) => {
-	if (Array.isArray(vector) && vector.length === 3) {
-		const [x, y, z] = vector.map(Number);
-		if (!isNaN(x) && !isNaN(y) && !isNaN(z)) {
-			return [x, y, z];
-		}
+const _resist = (force, amount) => {
+	const resistance = force * -amount;
+	if (
+		Math.sign(force + resistance) != Math.sign(force)
+		|| Math.abs(force) < 0.01
+	) return force * -1;
+	return resistance;
+}
+
+// base physics
+const physical = (init = () => { }) => {
+	let
+		position = [], velocity = [], acceleration = [],
+		hitboxes = [], forces = [],
+		mass = 1, invmass = 1 / mass, friction = 1, rotation = 0;
+
+	const setMass = num => {
+		mass = Math.max(0, Number(num) || 0);
+		invmass = 1 / mass;
+		return mass;
 	}
-	return [0, 0, 0];
-};
-
-// physical properties for tile entities, or blocks
-const tilePhysical = (initialData = [[0, 0], [0, 0], 1, 0]) => {
-	let [position, velocity, size, rotation] = initialData;
-	position = _validateVector(position);
-	velocity = _validateVector(velocity);
-	rotation = _snapRotation(rotation);
-
-	const setPos = ([x, y]) => { position = _validateVector([x, y]); };
-	const getPos = () => position;
-	const setPosObject = ({ x, y }) => { position = _validateVector([x, y]); };
-	const getPosObject = () => ({ x: position[0], y: position[1] });
-
-	const setVel = ([vx, vy]) => { velocity = _validateVector([vx, vy]); };
-	const getVel = () => velocity;
-	const setVelObject = ({ vx, vy }) => { velocity = _validateVector([vx, vy]); };
-	const getVelObject = () => ({ vx: velocity[0], vy: velocity[1] });
-
-	const getSize = () => size;
-	const setSize = (newSize) => { size = newSize; };
+	const getMass = () => mass;
 
 	const getRotation = () => rotation;
-	const setRotation = (newRotation) => { rotation = _snapRotation(newRotation); };
+	const setRotation = num => rotation = _rectifyRotation(Number(num) || 0);
 
-	const applyVelocity = () => {
-		position[0] += velocity[0];
-		position[1] += velocity[1];
+	const setPosition = vec => position = _validateVector(vec);
+	const getPosition = () => position;
+
+	const setVelocity = vec => velocity = _validateVector(vec);
+	const getVelocity = () => velocity;
+	const applyVelocity = () => position = position.map((p, i) => p + (velocity[i] || 0));
+
+	const setAcceleration = vec => acceleration = _validateVector(vec);
+	const getAcceleration = () => acceleration;
+	const applyAcceleration = () => {
+		velocity = velocity.map((v, i) => v + (acceleration[i] || 0));
+		acceleration.fill(0);
+		return velocity;
 	};
 
-	const applyFriction = () => {
-		velocity = [0, 0];
+	const setFriction = (num, mult = false) => friction = Math.max(0, mult ? 1 - num : Number(num) || 0);
+	const getFriction = () => friction;
+	const applyFriction = () => velocity = velocity.map(v => v + _resist(v, friction))
+
+	const addForce = (vec, duration = 0) => forces.push([_validateVector(vec), Number(duration) || 0]);
+	const getForces = () => forces;
+	const applyForces = () => forces.forEach(force => {
+		acceleration = acceleration.map((a, i) => i > force[0].length ? a : a + force[0][i] * invmass);
+		force[1]--;
+		if (force[1] <= 0) forces.splice(forces.indexOf(force), 1);
+	});
+
+	const resizePhysicalDimensions = n => {
+		position = Array(n).fill(0);
+		velocity = Array(n).fill(0);
+		acceleration = Array(n).fill(0);
+	}
+
+	const raw = () => [position, velocity, acceleration, hitboxes, forces];
+
+	const api = {
+		setMass, getMass,
+		getRotation, setRotation,
+		setPosition, getPosition,
+		setVelocity, getVelocity, applyVelocity,
+		setAcceleration, getAcceleration, applyAcceleration,
+		setFriction, getFriction, applyFriction,
+		addForce, getForces, applyForces,
+		resizePhysicalDimensions,
+		raw
 	};
+
+	init(api);
+
+	return api;
+}
+
+// physical properties for tile entities, or blocks
+const tilePhysical = (init = () => { }) => {
+	const phys = physical(p => {
+		p.resizePhysicalDimensions(2);
+		p.setFriction(1);
+	});
+
+	let size = 1;
+
+	const setPosition = ([x, y]) => phys.setPosition([x, y]);
+	const setPositionObject = ({ x, y }) => phys.setPosition([x, y]);
+	const getPositionObject = () => {
+		const pos = phys.getPosition();
+		return { x: pos[0], y: pos[1] };
+	};
+
+	const setVelocity = ([vx, vy]) => phys.setVelocity([vx, vy]);
+	const setVelocityObject = ({ vx, vy }) => phys.setVelocity([vx, vy]);
+	const getVelocityObject = () => {
+		const vel = phys.getVelocity();
+		return { x: vel[0], y: vel[1] };
+	};
+
+	const setSize = newSize => size = newSize;
+	const getSize = () => size;
+
+	const setRotation = newRotation => phys.setRotation(_snapRotation(newRotation));
 
 	const tick = () => {
-		applyVelocity();
-		applyFriction();
+		phys.applyForces();
+		phys.applyAcceleration();
+		phys.applyVelocity();
+		phys.applyFriction();
 	};
 
-	const raw = () => [position, velocity, size, rotation];
+	const raw = () => phys.raw().concat(size);
 
-	return {
-		setPos, getPos,
-		setPosObject, getPosObject,
-		setVel, getVel,
-		setVelObject, getVelObject,
+	const api = {
+		...phys,
+
+		setPosition,
+		setPositionObject, getPositionObject,
+		setVelocity,
+		setVelocityObject, getVelocityObject,
 		setSize, getSize,
-		setRotation, getRotation,
-		applyVelocity, applyFriction,
+		setRotation,
 		tick,
 		raw,
 
 		type: "physical:tile"
 	};
+
+	init(api);
+
+	return api;
 };
 
 // physical properties for free entities, or pods
-const freePhysical = (initialData = [[0, 0, 0], [0, 0, 0], 10, 0.99, 0.1, 100, 0]) => {
-	let [position, velocity, size, friction, gravity, life, rotation] = initialData;
-	position = _validateVector3D(position);
-	velocity = _validateVector3D(velocity);
-	rotation = _rectifyRotation(rotation);
+const freePhysical = (init = () => { }) => {
+	const phys = physical(p => {
+		p.resizePhysicalDimensions(3);
+		p.setFriction(0.01);
+	});
 
-	const setPos = ([x, y, z]) => { position = _validateVector3D([x, y, z]); };
-	const getPos = () => position;
-	const setPosObject = ({ x, y, z }) => { position = _validateVector3D([x, y, z]); };
-	const getPosObject = () => ({ x: position[0], y: position[1], z: position[2] });
+	let size = 10, gravity = 0.1;
 
-	const setVel = ([vx, vy, vz]) => { velocity = _validateVector3D([vx, vy, vz]); };
-	const getVel = () => velocity;
-	const setVelObject = ({ vx, vy, vz }) => { velocity = _validateVector3D([vx, vy, vz]); };
-	const getVelObject = () => ({ vx: velocity[0], vy: velocity[1], vz: velocity[2] });
+	const setPosition = ([x, y, z]) => (phys.setPosition([x, y, z]));
+	const setPositionObject = ({ x, y, z }) => phys.setPosition([x, y, z]);
+	const getPositionObject = () => {
+		const pos = phys.getPosition();
+		return { x: pos[0], y: pos[1], x: pos[2] };
+	};
+
+	const setVelocity = ([vx, vy, vz]) => phys.setVelocity([vx, vy, vz]);
+	const setVelocityObject = ({ vx, vy, vz }) => phys.setVelocity([vx, vy, vz]);
+	const getVelocityObject = () => {
+		const vel = phys.getVelocity();
+		return { x: vel[0], y: vel[1] }; ocity
+	};
 
 	const getSize = () => size;
-	const setSize = (newSize) => { size = newSize; };
+	const setSize = newSize => size = newSize;
 
-	const getFriction = () => friction;
-	const setFriction = (newFriction) => { friction = newFriction; };
-
-	const getGrav = () => gravity;
-	const setGrav = (newGravity) => { gravity = newGravity; };
-
-	const getRotation = () => rotation;
-	const setRotation = (newRotation) => { rotation = _rectifyRotation(newRotation); };
-
-	const applyVelocity = () => {
-		position[0] += velocity[0];
-		position[1] += velocity[1];
-		position[2] += velocity[2];
-	};
-
-	const applyGravity = () => {
-		velocity[2] -= gravity; // Assuming gravity affects the z-axis
-	};
-
-	const applyFriction = () => {
-		velocity = velocity.map(v => v * friction);
-	};
+	const getGravity = () => gravity;
+	const setGravity = newGravity => gravity = newGravity;
+	const applyGravity = () => phys.addForce([0, 0, -gravity || 0], 1);
 
 	const tick = () => {
 		applyGravity();
-		applyVelocity();
-		applyFriction();
+		phys.applyForces();
+		phys.applyAcceleration();
+		phys.applyVelocity();
+		phys.applyFriction();
 	};
 
-	const raw = () => [position, velocity, size, friction, gravity, life, rotation];
+	const raw = () => phys.raw().concat(gravity, size);
 
-	return {
-		setPos, getPos,
-		setPosObject, getPosObject,
-		setVel, getVel,
-		setVelObject, getVelObject,
+	const api = {
+		...phys,
+
+		setPosition, setPositionObject, getPositionObject,
+		setVelocity, setVelocityObject, getVelocityObject,
 		getSize, setSize,
-		getFriction, setFriction,
-		getGrav, setGrav,
-		getRotation, setRotation,
-		applyVelocity, applyGravity, applyFriction,
+		getGravity, setGravity, applyGravity,
 		tick,
 		raw,
 
 		type: "physical:free"
 	};
+
+	init(api);
+
+	return api;
 };
 
 export { tilePhysical, freePhysical };
